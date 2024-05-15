7a3b0f5682e40f0613ec10a2900a2c0e960e41e6
Matt Ryan <msr1222@gmail.com>
2024-05-15 15:29:49 -0400

    Remove unused custom blocks

--- patterns/template-biography.php
+++ patterns/template-biography.php
@@ -5,26 +5,20 @@
  * Block Types: core/post-content
  * Post Types: biography
  * 
- * This template  is offered in the pattern selector when Ad dNew is clicked. 
+ * This template  is offered in the pattern selector when Add New Biography is clicked. 
  */
 
 ?>
 <!-- wp:paragraph {"align":"center", "backgroundColor":"theme-palette8"} -->
-Comment 1. Add title, taxonomies on the side, and fill in the biography details below the sample CDV.
-Save draft. Scroll to next comment to enter biography content. Remove this comment before publication.
+<strong>Biography Post Instructions</strong>
+Add Biography title & Save draft. 
+Select taxonomies on the right side
+(Municipalities, Working Decades, Processes).
+Set biography details in the Biography Details meta box below. 
+Scroll to next comment, select all, 
+and begin entering your biography content.
+The first paragraph of your biography 
+should be the post excerpt. 
+Remove this comment before publication.
 <!-- /wp:paragraph -->
 <!-- wp:block {"ref":1837} /-->
-
-<!-- wp:kadence/rowlayout {"columns":1,"colLayout":"equal","kbVersion":2} -->
-<!-- wp:kadence/column {"borderWidth":["","","",""],"kbVersion":2} -->
-<div class="wp-block-kadence-column"><div class="kt-inside-inner-col">
-<!-- wp:paragraph {"align":"center", "backgroundColor":"theme-palette8"} -->
-Comment 2. Insert biography text copy here. Copy/paste. Remove this text before publication. 
-Sample CDV block below. Duplicate as needed.
-<!-- /wp:paragraph -->
-</div></div>
-<!-- /wp:kadence/column -->
-<!-- /wp:kadence/rowlayout -->
-
-<!-- wp:pattern {"slug":"ChesterCountyPhotographist/template-cdv"} /-->
-
