0299c20eab89c209936dfa315e2e23cae9c52219
Matt Ryan <msr1222@gmail.com>
2024-07-11 12:05:17 -0400

    Update biography CPT template to remove instructional text.

--- patterns/template-biography.php
+++ patterns/template-biography.php
@@ -9,17 +9,5 @@
  */
 
 ?>
-<!-- wp:paragraph {"align":"center", "backgroundColor":"theme-palette8"} -->
-<strong>Biography Post Instructions</strong>
-Add Biography title & Save draft. 
-Select taxonomies on the right side
-(Municipalities, Working Decades, Processes).
-Set biography details in the Biography Details meta box below. 
-USe the Block insert below to begin entering your biography content.
-The first paragraph of your biography 
-is the post excerpt. 
-The actual content of the biography will begin with 
-the 2nd paragraph of the biography content. 
-Remove this comment before publication.
-<!-- /wp:paragraph -->
+
 <!-- wp:block {"ref":1837} /-->
